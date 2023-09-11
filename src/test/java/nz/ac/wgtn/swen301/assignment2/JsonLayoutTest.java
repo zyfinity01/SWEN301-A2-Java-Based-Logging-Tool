e8cffcfd7fe9d83555895d83c7bb69e4affbbca0
Niraj Gandhi <gandhinira@myvuw.ac.nz>
2023-09-12 00:27:40 +1200

    testing JsonLayout

--- src/test/java/nz/ac/wgtn/swen301/assignment2/JsonLayoutTest.java
+++ src/test/java/nz/ac/wgtn/swen301/assignment2/JsonLayoutTest.java
@@ -1,4 +1,58 @@
 package nz.ac.wgtn.swen301.assignment2;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Category;
+import org.apache.log4j.Priority;
+import org.apache.log4j.spi.LocationInfo;
+import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.ThrowableInformation;
+import org.junit.jupiter.api.Test;
+import org.apache.log4j.Level;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class JsonLayoutTest {
-}
+
+    @Test
+    public void test1(){
+        Category logger = Category.getInstance("foo");
+        Instant instant = Instant.parse("2011-12-03T10:15:30Z");
+        long timeStamp = instant.toEpochMilli();
+        Level level = Level.WARN;
+        Object message = "something went wrong";
+        String threadName = "main";
+        ThrowableInformation throwable = new ThrowableInformation(new Exception("Test exception"), logger);
+        String ndc = "NDC";
+        LocationInfo info = new LocationInfo(new Throwable("Test throwable"), "FQN");
+        Map properties = new HashMap();
+        LoggingEvent le = new LoggingEvent("FQN", logger, timeStamp, level, message, threadName, throwable, ndc, info, properties);
+        String json = new JsonLayout().format(le);
+        String expected = "{" +
+                "\"name\":\"foo\"," +
+                "\"level\":\"WARN\"," +
+                "\"timestamp\":\"2011-12-03T10:15:30Z\"," +
+                "\"thread\":\"main\"," +
+                "\"message\":\"something went wrong\"" +
+                "},";
+        //System.out.println(json);
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode expectedNode = null;
+        JsonNode actualNode = null;
+        try {
+            expectedNode = objectMapper.readTree(expected);
+            actualNode = objectMapper.readTree(json);
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(expectedNode, actualNode);
+    }
+    }
+
