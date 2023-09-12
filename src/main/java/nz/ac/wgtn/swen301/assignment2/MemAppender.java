2a85017143e89924aaee4364e57a4fe21415aa25
Niraj Gandhi <ngandhi360@gmail.com>
2023-09-12 14:45:48 +1200

    Create MemAppender class

--- src/main/java/nz/ac/wgtn/swen301/assignment2/MemAppender.java
+++ src/main/java/nz/ac/wgtn/swen301/assignment2/MemAppender.java
@@ -1,4 +1,18 @@
 package nz.ac.wgtn.swen301.assignment2;
+import org.apache.log4j.pattern.LogEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MemAppender {
+
+    private List<LogEvent> events = new ArrayList<>();
+
+    String name;
+
+
+
+    public MemAppender(String name) {
+        this.name = name;
+    }
+
 }
