1d28b5e95ee330f1f010f513ca4b9d5d04ad6f43
Niraj Gandhi <ngandhi360@gmail.com>
2023-09-13 13:56:36 +1200

    Implement test to test getting logs

--- src/test/java/nz/ac/wgtn/swen301/assignment2/MemAppenderTest.java
+++ src/test/java/nz/ac/wgtn/swen301/assignment2/MemAppenderTest.java
@@ -1,13 +1,16 @@
 package nz.ac.wgtn.swen301.assignment2;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.log4j.spi.LoggingEvent;
 import org.junit.jupiter.api.Test;
 import org.apache.log4j.Logger;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -17,7 +20,7 @@ public class MemAppenderTest {
     private static final Logger logger = Logger.getLogger(MemAppenderTest.class);
 
     @Test
-    public void test1(){
+    public void testExportToJson(){
         MemAppender memAppender = new MemAppender();
         memAppender.setName("test1");
         logger.addAppender(memAppender);
@@ -34,5 +37,20 @@ public class MemAppenderTest {
         }
         assertTrue(Files.exists(path));
         }
+
+    @Test
+    public void testGetCurrentLogs(){
+        MemAppender memAppender = new MemAppender();
+        Logger logger = Logger.getLogger(MemAppenderTest.class);
+        logger.addAppender(memAppender);
+
+        logger.info("This is an info message.");
+        logger.error("This is an error message.");
+
+        List<LoggingEvent> logs = memAppender.getCurrentLogs();
+        assertEquals(2, logs.size());
+        assertEquals("This is an info message.", logs.get(0).getMessage());
+        assertEquals("This is an error message.", logs.get(1).getMessage());
+    }
 
 }
