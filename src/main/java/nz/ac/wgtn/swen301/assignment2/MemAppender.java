7b656614832642e052fc8961c1372f71aa2e4fc5
Niraj Gandhi <gandhinira@myvuw.ac.nz>
2023-09-16 22:57:45 +1200

    implement close MBean

--- src/main/java/nz/ac/wgtn/swen301/assignment2/MemAppender.java
+++ src/main/java/nz/ac/wgtn/swen301/assignment2/MemAppender.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 public class MemAppender extends AppenderSkeleton implements MemAppenderMBean {
 
-    private List<LoggingEvent> events = new ArrayList<>();
+    private final List<LoggingEvent> events = new ArrayList<>();
 
     String name;
     Long maxSize = 1000L;
@@ -50,11 +50,7 @@ public class MemAppender extends AppenderSkeleton implements MemAppenderMBean {
         }
         try {
             mbs.registerMBean(this, objectName);
-        } catch (InstanceAlreadyExistsException e) {
-            e.printStackTrace();
-        } catch (MBeanRegistrationException e) {
-            e.printStackTrace();
-        } catch (NotCompliantMBeanException e) {
+        } catch (InstanceAlreadyExistsException | MBeanRegistrationException | NotCompliantMBeanException e) {
             e.printStackTrace();
         }
     }
@@ -115,7 +111,7 @@ public class MemAppender extends AppenderSkeleton implements MemAppenderMBean {
         }
 
         try {
-            objectMapper.writeValue(new File(this.name + ".json"), jsonTree);
+            objectMapper.writeValue(new File(filename + ".json"), jsonTree);
             System.out.println("JSON file created successfully");
         } catch (IOException e) {
             e.printStackTrace();
@@ -126,7 +122,14 @@ public class MemAppender extends AppenderSkeleton implements MemAppenderMBean {
 
     @Override
     public void close() {
-
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName objectName;
+        try {
+            objectName = new ObjectName("nz.ac.wgtn.swen301.assignment2:type=MemAppender,name=" + this.name);
+            mbs.unregisterMBean(objectName);
+        } catch (MalformedObjectNameException | InstanceNotFoundException | MBeanRegistrationException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
